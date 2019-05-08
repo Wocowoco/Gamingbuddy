a36d04de681faaa9ac05c88e1175535053e67ea9
joeki6 <matthias.hernalsteen@skynet.be>
2019-05-08 12:34:40 +0200

    blokjes + tekstblok

--- profielaanpas.php
+++ profielaanpas.php
@@ -32,7 +32,7 @@
             WHERE accountID = '$id'";
             $result = $conn->query($sql);
             if ($result->num_rows > 0) {
-                while($row = $result->fetch_assoc()) {
+                while($row = $result->fetch_assoc()) {       // zorgt dat ge 1 lijn hebt
                     $_SESSION['lol_RankID'] = $row['RankID'];
                 }
             }
@@ -41,3 +41,92 @@
         $conn->close();
     }
 ?> 
+
+
+<?php
+    function getnaam(){
+        $servername = "localhost";
+        $dbusername = "root";
+        $dbpassword = "";
+        $dbname = "gamingbuddy";
+
+        //ProfielID die je nu opzoekt
+            $id = 1;
+
+        // Create connection
+        $conn = new mysqli($servername, $dbusername, $dbpassword, $dbname);
+
+        // Check connection
+        if ($conn->connect_error) {
+            die("Connection failed: " . $conn->connect_error);
+        } 
+
+        //If logged in
+        if($id != null)
+        {
+            $sql = "SELECT Username
+            FROM gb_account 
+            WHERE ID = '$id'";
+            $result = $conn->query($sql);
+            if ($result->num_rows > 0) {
+                while($row = $result->fetch_assoc()) {       // zorgt dat ge 1 lijn hebt
+                    $_SESSION['zelfUsername'] = $row['Username'];
+                }
+            }
+        }
+
+        $conn->close();
+    }
+?> 
+<?php
+function getlolacounts(){
+
+    $servername = "localhost";
+    $dbusername = "root";
+    $dbpassword = "";
+    $dbname = "gamingbuddy";
+
+    $id = 1;
+     // Create connection
+     $conn = new mysqli($servername, $dbusername, $dbpassword, $dbname);
+
+     // Check connection
+     if ($conn->connect_error) {
+         die("Connection failed: " . $conn->connect_error);
+     } 
+
+                $number = 0;
+                //Prepared statement
+                $stmt = $conn->prepare("SELECT SummonerName, Zone, lolrank.Name AS rankName, lolrank.rankID AS rankNr, lolrole1.Name AS role1, lolrole2.Name AS role2
+                FROM gb_loldata AS loldat
+                INNER JOIN gb_lolrank AS lolrank ON loldat.RankID = lolrank.rankID
+                INNER JOIN gb_lolRole AS lolrole1 ON loldat.PrefRole1 = lolrole1.roleID
+                INNER JOIN gb_lolRole AS lolrole2 ON loldat.PrefRole2 = lolrole2.roleID
+                WHERE loldat.AccountID LIKE ?");
+                $stmt->bind_param("i",$id);
+                mysqli_stmt_execute($stmt);
+                $result = mysqli_stmt_get_result($stmt);
+
+                $number = 0;
+                while($row = $result->fetch_assoc())
+                    {
+                        
+                        $lolSummonerName = $row["SummonerName"];
+                        $lolZone = $row["Zone"];
+                        $lolrankName = $row["rankNr"];
+                        $lolRole1 = $row["role1"];
+                        $lolRole2 = $row["role2"];
+                        
+                        $loldata = array($lolSummonerName,$lolZone,$lolrankName,$lolRole1,$lolRole2);
+                        $_SESSION["loldata"][$number] = $loldata;
+                        $number++;
+
+                        
+                        }
+        
+                        $_SESSION['loldataAmount'] = $number;
+               
+
+        $conn->close();
+        }
+?>
