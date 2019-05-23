f9d9d787fcc44a96aef97b828425f3d212032943
joeki6 <matthias.hernalsteen@skynet.be>
2019-05-23 15:21:14 +0200

    nogmeer verwijderd

--- javascript/scripts.js
+++
@@ -1,5 +0,0 @@
-$.get("api.php", {url: "https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/joeki6?api_key=RGAPI-96885e71-99e3-4c77-b6c5-4f5c1d9868ed"}, 
-  data => {
-    data = JSON.parse(data)
-    console.log(data)
-  })
