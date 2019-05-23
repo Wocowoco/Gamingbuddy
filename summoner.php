91371ecc354851f8cf23c55dad51c0647f85a282
joeki6 <matthias.hernalsteen@skynet.be>
2019-05-23 11:36:48 +0200

    apikey

--- summoner.php
+++ summoner.php
@@ -4,7 +4,7 @@
     
 
     if (isset($_GET['endpoint'])) {
-        $apiKey = "RGAPI-1131dcbe-d284-463e-b8d8-b539c273e869";
+        $apiKey = "RGAPI-4562f1b6-9afa-45c5-9ff6-c836ab126d4e";
         $callUrl = $_GET['endpoint'] . "?api_key=" . $apiKey;
         echo file_get_contents($callUrl);
 
